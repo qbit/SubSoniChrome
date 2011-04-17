dffdece1af6051a383cc179bfd36a1259853160f
Aaron Bieber <qbit@deftly.net>
2011-04-17 15:59:28 -0600

    switched to jquery..

--- Subsonic.js
+++ Subsonic.js
@@ -1,54 +1,115 @@
 var Subsonic = {
 	serverStatus: false, 
+	serverLoaded: false,
 	serverVersion: '1.5.0',
+	serverName: "SubSonicChrome",
+	loadedUrl: '',
+	hasMsg: false,
 
-	gets: function( type ) {
+	reqs: {
+		artists: "/rest/getIndexes.view"
+	},
+
+	colors: {
+		green: "#32CD32",
+		white: "#ffffff",
+		red: "#FF4500"
+	},
+
+	loadServer: function( serverObj ) {
+
+		if ( ! serverObj.password ) {
+			log( "need password" );
+			// prompt for password
+		}
+
+		var artists = serverObj.server 
+			+ this.reqs['artists'] 
+			+ "?u=" + serverObj.username  
+			+ "&p=" + serverObj.password 
+			+ "&v=" + this.serverVersion
+			+ "&c=" + this.serverName
+			+ "&f=json";
+
+		$.ajax( {
+			url: artists,
+			success: function( data ) {
+				// draw tree view here
+			}
+		});
 	},
 
 	listServers: function() {
-		return localStorage['servers'] || [];
+		if ( localStorage.length === 0 ) {
+			return [];
+		} else {
+			return JSON.parse( localStorage.servers );
+		}
+	},
+
+	notify: function( msg, color ) {
+		log( "Showing notification: '" + msg + "' with color of '" + Subsonic.colors[color] + "'" );
+		$('#notification').slideDown( 'slow' );
+		$('#notification').html( msg );
+		$('#notification').css( "background-color", Subsonic.colors[color] ); 
+
+		this.msgTimer = setTimeout( function() {
+			$('#notification').slideUp( 'slow' );
+			Subsonic.hasMsg = false;
+		}, 5000 );
 	},
 
 	testServer: function( serverObj ) {
-		function success ( ret ) {
-			Subsonic.serverStatus = true;
-			console.log( "Success!" + ret );
-		}
 
-		function fail( ret ) {
-			Subsonic.serverStatus = false;
-			console.log( "Failed!" + ret );
-		}
+		var url = serverObj.server + "/rest/ping.view?u=" + serverObj.username + "&p=" + serverObj.password + "&c=" + Subsonic.serverName + "&f=json&v=" + Subsonic.serverVersion;
 
-		var url = serverObj.server + "/rest/ping.view?u=" + serverObj.username + "&p=" + serverObj.password + "&c=SubSoniChrome&f=json&v=" + Subsonic.serverVersion;
-		console.log( "Testing: " + url );
-
-		Ajax.init( url, 'Subsonic' );
-		Ajax.make_request( 
-			success,
-			fail
-		);
+		// switch to jquery
+		$.ajax({
+			url: url,
+			jsonp: true,
+			success: function( data ) {
+				if ( data['subsonic-response'].status === "ok" ) {
+					Subsonic.serverStatus = true;
+					Subsonic.notify( "Success!", 'green' );
+				} else {
+					Subsonic.notify( "Failed: " + data['subsonic-response'].error.message, 'red' );
+				}
+			},
+			error: function( data ) {
+				Subsonic.serverStatus = false;
+				Subsonic.notify( "Failed: " + data, 'red' );
+			}
+		});
 	},
 
 	saveServer: function( serverObj ) {
-		console.log( "Saving..." );
-		if ( ! localStorage['servers'] ) {
-			localStorage['servers'] = [];
+		if ( ! localStorage.servers ) {
+			localStorage.servers = [];
 		}
 
 		if ( serverObj.server && serverObj.username && serverObj.password) {
-			var results = Subsonic.testServer( serverObj );
-			if ( results.success ) {
-				if ( serverObj.save === true ) {
-					localStorage['servers'].push( serverObj );
-				} else {
-					delete serverObj.password
-					localStorage['servers'].push( serverObj );
+
+			Subsonic.testServer( serverObj );
+
+			var servers = [];
+
+			setInterval( function() {
+				if ( Subsonic.serverStatus === true ) {
+
+					if ( serverObj.save === true ) {
+						delete serverObj.save;
+						servers.push( serverObj );
+					} else {
+						delete serverObj.save;
+						delete serverObj.password
+						servers.push( serverObj );
+					}
+
+					localStorage['servers'] = JSON.stringify( servers );
+					Subsonic.serverStatus = false;
 				}
-				return true;
-			} else {
-				return results;
-			}
+			}, 100 );
+
 		}
 	},
 
